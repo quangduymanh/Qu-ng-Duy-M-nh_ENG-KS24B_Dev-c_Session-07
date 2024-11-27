c709279f7062c8a304d3926eae5859170c5766ea
quangduymanh <quangduymanh4@gmail.com>
2024-11-27 23:38:39 +0700

    Update session 07- bài 10.cpp

--- session 07- bài 10.cpp
+++ session 07- bài 10.cpp
@@ -1,15 +1,21 @@
 #include <stdio.h>
 int isPrime(int n) {
     if (n <= 1) return 0;
-    for (int i = 2; i * i <= n; i++) if (n % i == 0) return 0;
+    for (int i = 2; i * i <= n; i++) 
+        if (n % i == 0) return 0;
     return 1;
 }
 int main() {
     int n;
+    printf("Nhap so luong phan tu: ");
     scanf("%d", &n);
     int a[n];
-    for (int i = 0; i < n; i++) scanf("%d", &a[i]);
-    for (int i = 0; i < n; i++) if (isPrime(a[i])) printf("%d ", a[i]);
-    printf("\n");
+    printf("Nhap cac phan tu cua mang:\n");
+    for (int i = 0; i < n; i++) 
+        scanf("%d", &a[i]);
+    printf("Cac so nguyen to trong mang: ");
+    for (int i = 0; i < n; i++) 
+        if (isPrime(a[i])) 
+            printf("%d ", a[i]);
     return 0;
 }
